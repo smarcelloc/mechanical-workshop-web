1f1bd874717a29a358cb5c4877da2132ea10a5de
Marcello Silvério <smarcelloc@gmail.com>
2025-04-10 18:12:24 -0300

    refactor(layout): update page title and add dark mode class
    Update the page title to "Oficina Mecânica" to reflect the correct branding. Add the "dark" class to the HTML element to enable dark mode styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,16 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Workshop Mechanical",
+  title: "Oficina Mecânica",
   description: "Workshop Mechanical",
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <html lang="pt-br" suppressHydrationWarning>
+    <html
+      lang="pt-br"
+      className="dark"
+      suppressHydrationWarning>
       <body>{children}</body>
     </html>
   );
