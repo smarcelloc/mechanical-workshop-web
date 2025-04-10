1b55dec28e71abd35f7ef0d8adae664320f1a72c
Marcello Silvério <smarcelloc@gmail.com>
2025-04-10 18:12:38 -0300

    style(eslint): update rules and ignore specific directory
    Update ESLint rules to use "multiprop" for `react/jsx-first-prop-new-line` and add ignore pattern for `src/components/ui/**` to exclude it from linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,10 +12,11 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    ignores: ["src/components/ui/**"],
     rules: {
       "react/jsx-max-props-per-line": [1, { "maximum": 1 }],
       "react/jsx-closing-bracket-location": [1, "after-props"],
-      "react/jsx-first-prop-new-line": [1, "multiline"]
+      "react/jsx-first-prop-new-line": [1, "multiprop"]
     }
   }
 ];
