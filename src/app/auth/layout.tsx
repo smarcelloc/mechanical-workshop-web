93bb675e565bf28c7c449bd0d53a7878c9d27fff
Marcello Silvério <smarcelloc@gmail.com>
2025-04-10 19:27:57 -0300

    refactor(auth): replace `<a>` with `<Link>` for navigation
    Using Next.js's `<Link>` component improves client-side navigation and performance by pre-fetching linked pages

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,8 +1,7 @@
 import { CarIcon } from "lucide-react"
+import Link from "next/link"
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
-
-
     return (
         <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
             <div className="w-full max-w-sm">
@@ -10,15 +9,15 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
                     <div className="flex flex-col gap-6">
 
                         <div className="flex flex-col items-center gap-2">
-                            <a
-                                href="#"
+                            <Link
+                                href="/auth/login"
                                 className="flex flex-col items-center gap-2 font-medium"
                                 tabIndex={-1}>
                                 <div className="flex h-10 w-10 items-center justify-center rounded-md">
                                     <CarIcon className="size-dvh" />
                                 </div>
                                 <span className="sr-only">Logo do Sistema</span>
-                            </a>
+                            </Link>
                             <h1 className="text-xl font-bold">Oficina Mecânica</h1>
                         </div>
 
