c76bfe8833e914008863967ba8ed785e650658b5
Marcello Silvério <smarcelloc@gmail.com>
2025-04-12 15:25:26 -0300

    feat(login): add layout component for login page
    Introduce a new layout component for the login page to structure the UI, including a navigation link to the home page and a container for the login form

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,26 @@
+import { CarIcon } from 'lucide-react'
+import Link from 'next/link'
+
+export default function LoginLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <div className="grid min-h-svh">
+            <div className="flex flex-col gap-4 p-6 md:p-10">
+                <div className="flex justify-center gap-2 md:justify-start">
+                    <Link
+                        href="/"
+                        className="flex items-center gap-2 font-medium">
+                        <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+                            <CarIcon className="size-4" />
+                        </div>
+                        {process.env.APP_NAME}
+                    </Link>
+                </div>
+                <div className="flex flex-1 items-center justify-center">
+                    <div className="w-full max-w-xs">
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
