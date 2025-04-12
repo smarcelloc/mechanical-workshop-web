f4f97052ab2fc92ac11d3c4f4523bda30a4f0caa
Marcello Silvério <smarcelloc@gmail.com>
2025-04-12 14:43:21 -0300

    chore: update lint script and eslint configuration
    - Modify the lint script in package.json to include the --fix flag for automatic fixes
    - Add new ESLint rules for React components and TypeScript files, including formatting and prop handling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["src/**/*.tsx", "src/**/*.ts"],
+    ignores: ["src/components/ui/**"],
+    rules: {
+      "react/jsx-max-props-per-line": [1, { "maximum": 1 }],
+      "react/jsx-closing-bracket-location": [1, "after-props"],
+      "react/jsx-first-prop-new-line": [1, "multiprop"],
+      "semi": ["error", "never"],
+      "quotes": ["error", "single"],
+      "@typescript-eslint/no-explicit-any": "off",
+    }
+  }
 ];
 
 export default eslintConfig;
