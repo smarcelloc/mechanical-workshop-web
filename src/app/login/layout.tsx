6f0268fd5244fbec0a852076126225551ef9c6ae
Marcello Silvério <smarcelloc@gmail.com>
2025-04-13 02:22:35 -0300

    style(login): adjust layout spacing and increase max-width for login form
    Improve readability and visual appeal by adding consistent spacing and increasing the max-width of the login form container from `max-w-xs` to `max-w-sm`.

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -5,6 +5,7 @@ export default function LoginLayout({ children }: Readonly<{ children: React.Rea
     return (
         <div className="grid min-h-svh">
             <div className="flex flex-col gap-4 p-6 md:p-10">
+
                 <div className="flex justify-center gap-2 md:justify-start">
                     <Link
                         href="/"
@@ -15,11 +16,13 @@ export default function LoginLayout({ children }: Readonly<{ children: React.Rea
                         {process.env.APP_NAME}
                     </Link>
                 </div>
+
                 <div className="flex flex-1 items-center justify-center">
-                    <div className="w-full max-w-xs">
+                    <div className="w-full max-w-sm">
                         {children}
                     </div>
                 </div>
+
             </div>
         </div>
     )
