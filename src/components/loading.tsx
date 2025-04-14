f28fbff92b6981ad5f0f21c00adfb5c6a9b35dd9
Marcello Silvério <smarcelloc@gmail.com>
2025-04-14 11:51:53 -0300

    style(loading): improve loading component layout and animation
    Refactor the loading component to enhance visual appeal by adjusting the layout, adding nested spinning animations, and removing redundant margins. The changes aim to provide a more polished and modern loading experience.

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -28,16 +28,23 @@ export default function Loading({
     if (!show || !shouldRender) return null
 
     return (
-        <div className="fixed inset-0 flex justify-center items-center z-9999 bg-black/95">
-            <div className="text-center">
-                <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-yellow-500 mx-auto"></div>
-                <h5 className="text-zinc-900 dark:text-white mt-4 font-bold">
+        <div className="fixed inset-0 flex flex-col gap-8 justify-center items-center z-9999 bg-black/95">
+
+            <div className="flex-col gap-4 w-full flex items-center justify-center">
+                <div className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full">
+                    <div className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"></div>
+                </div>
+            </div>
+
+            <div className="flex flex-col text-center gap-2">
+                <h5 className="text-zinc-900 dark:text-white font-bold">
                     {message}
                 </h5>
-                <p className="text-balance text-sm text-muted-foreground mt-2">
+                <p className="text-balance text-sm text-muted-foreground">
                     {description}
                 </p>
             </div>
+
         </div>
     )
 }
