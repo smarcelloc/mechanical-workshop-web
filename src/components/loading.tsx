b153032683e4901f5e678ec148739aad72147445
Marcello Silvério <smarcelloc@gmail.com>
2025-04-14 01:01:23 -0300

    feat(loading): add delay and conditional rendering to Loading component
    Introduce a delay and conditional rendering to the Loading component to improve user experience by avoiding immediate display of the loading spinner. This allows for smoother transitions and prevents unnecessary rendering when the loading state is not active.

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,20 +1,41 @@
-import React from 'react'
+'use client'
+
+import { useEffect, useState } from 'react'
 
 interface LoadingProps {
     message?: string;
     description?: string;
+    show?: boolean;
+    delay?: number;
 }
 
-export default function Loading({ message, description }: LoadingProps) {
+export default function Loading({
+    message = 'Carregando...',
+    description = 'Por favor, aguarde enquanto processamos sua solicitação',
+    show = true,
+    delay = 500
+}: LoadingProps) {
+    const [shouldRender, setShouldRender] = useState(false)
+
+    useEffect(() => {
+        let timeoutId: NodeJS.Timeout
+        if (show) timeoutId = setTimeout(() => setShouldRender(true), delay)
+        else setShouldRender(false)
+        return () => clearTimeout(timeoutId)
+    }, [show, delay])
+
+    // Se o loading não estiver ativo, não renderize nada
+    if (!show || !shouldRender) return null
+
     return (
         <div className="fixed inset-0 flex justify-center items-center z-9999 bg-black/95">
             <div className="text-center">
                 <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-yellow-500 mx-auto"></div>
                 <h5 className="text-zinc-900 dark:text-white mt-4 font-bold">
-                    {message || 'Carregando...'}
+                    {message}
                 </h5>
                 <p className="text-balance text-sm text-muted-foreground mt-2">
-                    {description || 'Por favor, aguarde enquanto processamos sua solicitação'}
+                    {description}
                 </p>
             </div>
         </div>
